7a227848a9c1b8375b96efdbcfd7caf7edece5eb
furkanfleur <furkanfleur@gmail.com>
2018-12-10 16:56:06 +0300

    Description Page Update (Most of layout done)

--- app/src/main/java/com/example/melahat/periodictable/ElementDetailActivity.java
+++ app/src/main/java/com/example/melahat/periodictable/ElementDetailActivity.java
@@ -1,9 +1,11 @@
 package com.example.melahat.periodictable;
 
 import android.os.Bundle;
+
+import com.example.melahat.periodictable.databinding.ActivityElementDetailBinding;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import com.example.melahat.periodictable.databinding.ActivityElementDetailBinding;
 
 public class ElementDetailActivity extends AppCompatActivity {
 
@@ -15,8 +17,68 @@ public class ElementDetailActivity extends AppCompatActivity {
         ActivityElementDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_element_detail);
 
         PeriodicTable.Element element = getIntent().getParcelableExtra("element");
+        String holder;
 
-        binding.textView.setText(element.name);
+        if (element.name != null) {
+            holder = element.name.substring(0, 1).toUpperCase() + element.name.substring(1);
+            binding.elementText.setText(holder);
+        } else {
+            binding.elementText.setText("-");
+        }
+
+        if (element.category != null) {
+            holder = element.category.substring(0, 1).toUpperCase() + element.category.substring(1);
+            binding.categoryText.setText(holder);
+        } else {
+            binding.elementText.setText("-");
+        }
+
+        if (element.appearance != null) {
+            holder = element.appearance.substring(0, 1).toUpperCase() + element.appearance.substring(1);
+            binding.appereanceText.setText(holder);
+        } else {
+            binding.appereanceText.setText("-");
+        }
+
+        if (element.discovered_by != null) {
+            holder = element.discovered_by.substring(0, 1).toUpperCase() + element.discovered_by.substring(1);
+            binding.discovererText.setText(holder);
+        } else {
+            binding.discovererText.setText("-");
+        }
+
+        if (element.named_by != null) {
+            holder = element.named_by.substring(0, 1).toUpperCase() + element.named_by.substring(1);
+            binding.namedText.setText(holder);
+        } else {
+            binding.namedText.setText("-");
+        }
+
+        if (element.atomic_mass != null) {
+            binding.massText.setText(element.atomic_mass.toString());
+        } else {
+            binding.massText.setText("-");
+        }
+
+        if (element.density != null) {
+            binding.densityText.setText(element.density.toString());
+        } else {
+            binding.densityText.setText("-");
+        }
+
+        if (element.boil != null) {
+            binding.boilText.setText(element.boil.toString());
+        } else {
+            binding.boilText.setText("-");
+        }
+
+        if (element.melt != null) {
+            binding.meltText.setText(element.melt.toString());
+        } else {
+            binding.meltText.setText("-");
+        }
+
+        binding.descriptionText.setText(element.summary);
 
     }
 }
